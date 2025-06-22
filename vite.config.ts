d8a4bff7324a045206d5084e3f8318bbaabedaec
Gene Luong <geneluong.landmark@gmail.com>
2025-06-22 12:01:20 +0000

    chore: added github actions to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 4202
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    server: {
+      host: '0.0.0.0',
+      port: 4202
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: {
+      APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    },
+    base: process.env.NODE_ENV === 'production' ? '/cs-course-tools' : '/'
   }
 })
