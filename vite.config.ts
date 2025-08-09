05c8c6df621fccb83b037191d5a70be9cc9dfe75
Gene Luong <geneluong.landmark@gmail.com>
2025-08-09 11:09:16 +0000

    chore: added wrangler integration

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { cloudflare } from "@cloudflare/vite-plugin"
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      cloudflare(),
+    ],
     server: {
       host: '0.0.0.0',
       port: 4202
