a2d98c18eac72c956f8bcfe8288b7824fee7c4cf
Gene Luong <geneluong.landmark@gmail.com>
2025-10-26 12:55:16 +0000

    fix: unit testing config merging

--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,24 @@ import vue from '@vitejs/plugin-vue'
 import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [
-      vue(),
-      cloudflare(),
-    ],
-    server: {
-      host: '0.0.0.0',
-      port: 4202
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    define: {
-      APP_VERSION: JSON.stringify(process.env.npm_package_version),
-    },
-    base: process.env.NODE_ENV === 'production'
-      ? process.env.IS_WRANGLER === 'TRUE' ? '/' : '/cs-course-tools'
-      : '/'
-  }
+export default defineConfig({
+  plugins: [
+    vue(),
+    cloudflare(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 4202
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? process.env.IS_WRANGLER === 'TRUE' ? '/' : '/cs-course-tools'
+    : '/'
 })
